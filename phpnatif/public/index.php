44f094903c16fd965f38af4ec76bc291466d427a
DEVnahjo <nahjo@nahjo.home>
2019-03-08 17:46:46 +0100

    add php natif

--- phpnatif/public/index.php
+++ phpnatif/public/index.php
@@ -33,24 +33,58 @@ $connectionParams = [
 // la variable `$conn` permet de communiquer avec la BDD
 $conn = DriverManager::getConnection($connectionParams, $config);
 
+// recuperation de la table zone
 $reponse = $conn->query("SELECT * FROM zone");
 while($req = $reponse->fetch()){
     $zones[] = $req['name'];
 }
 
-if($_POST){
-    $zone_select = $_POST["zone_lol"];
-    echo 'ok';
+// recuperation de la table planche
+$reponse = $conn->query("SELECT * FROM planche");
+while($req = $reponse->fetch()){
+    $planches[] = $req['name'];
 }
 
-// if($zone_select == "Jardin"){
-//     $reponse = $conn->query("SELECT * FROM planche");
-//     while($req = $reponse->fetch()){
-//         $planches[] = $req['name'];
-//     }
-// }
+
+// recuperation de la table serre
+$reponse = $conn->query("SELECT * FROM serre");
+while($req = $reponse->fetch()){
+    $serres[] = $req['name'];
+}
+
+// recuperation de la table legume
+$reponse = $conn->query("SELECT * FROM legume");
+while($req = $reponse->fetch()){
+    $legumes[] = $req['name'];
+}
+
+// recuperation de la table tache
+$reponse = $conn->query("SELECT * FROM tache");
+while($req = $reponse->fetch()){
+    $taches[] = $req['name'];
+}
 
 
+if($_POST){
+    $select_zone = $_POST["zone"];
+    $select_planche = $_POST["planche"];
+    $select_serre = $_POST["serre"];
+    $select_legume = $_POST["legume"];
+    $select_tache = $_POST["tache"];   
+}
+
+if($select_zone == "Selectionnez une zone" Or $select_legume == "Selectionnez un legume" Or $select_tache == "Selectionnez une tache" ){
+    $erreur = "Veuillez remplir tout les champs";
+} else {
+    $req = $conn->prepare('INSERT INTO rotation(zone , number_serre, number_planche ,legume, tache) VALUES(:zone ,:number_serre, :number_planche ,:legume , :tache )');
+    $req->execute(array(
+        'zone' => $select_zone,
+        'number_serre' => $select_serre,
+        'number_planche' => $select_planche,
+        'legume' => $select_legume,
+        'tache' => $select_tache,
+    ));
+}
 
 
     
@@ -59,5 +93,9 @@ echo $twig->render('index.html.twig', [
 // transmission de données au template
     'zone_select' => $zone_select,
     'zones' => $zones,
-    // 'planches' => $planches
+    'planches' => $planches,
+    'serres' => $serres,
+    'legumes' => $legumes,
+    'taches' => $taches,
+    'erreur' => $erreur,
 ]);
--- phpnatif/public/js/main.js
+++ phpnatif/public/js/main.js
@@ -1,10 +1,37 @@
-var select = document.querySelector(".zone");
-select.addEventListener("change", zone);
+var select_zone = document.querySelector(".zone");
+select_zone.addEventListener("change", zone);
 
-function zone(){
+var select_planche = document.querySelector(".planche");
+var select_serre = document.querySelector(".serre");
+var select_tache = document.querySelector(".tache");
+select_tache.addEventListener("change" , tache);
+var check = document.querySelector(".form-check");
 
-    if(select.value == "Jardin"){
+function zone(event){
+    event.preventDefault();
+    if(select_zone.value == "Jardin"){
         console.log("okook");
+        select_planche.style.display = "inline";
+    }else {
+        select_planche.style.display = "none" ;
+        select_planche.value = null;
+    }
+
+    if (select_zone.value == "Serre"){
+        select_serre.style.display = "inline";
+    }else {
+        select_serre.style.display = "none" ;
+        select_serre.value = null;
     }
     
+}
+
+
+function tache() {
+
+    if(select_tache.value == "Couvert"){
+        check.style.display = "block";
+    }else {
+        check.style.display = "none";
+    }
 }
